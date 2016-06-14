d27c7cbb4fac0a7aea3ad215e6f491c734d304dc
jenmcphail <mcphail.jen@gmail.com>
2016-06-14 16:06:10 -0700

    completes lab. adds form and post route

---
+++ app/controllers/records_controller.rb
@@ -0,0 +1,28 @@
+class RecordsController < ApplicationController
+  def index
+    @records = Record.all
+    # render :index
+  end
+
+  def show
+    @record = Record.find(params[:id])
+    render :show #optional
+  end
+
+  def new
+    @record = Record.new
+    render :new #optional
+  end
+
+  def create
+    Record.create(record_params)
+      redirect_to('/records')
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:title, :artist, :year, :cover_art, :song_count)
+  end
+
+end
